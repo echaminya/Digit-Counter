901cdcfc27c76ec786d158bbd0c879c986fc7e76
echaminya <jmbarani@gmail.com>
2022-08-30 03:34:09 +0300

    Add files via upload

---
+++ DigitCalculator.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number;
+	cout << "Number:";
+	cin >> number;
+	if (number == 0)
+		cout << "You have entered 0 \n";
+	else {
+		if (number < 0)
+			number = -1 * number;
+		int counter = 0;
+		while (number > 0) {
+			number /= 10;
+			counter++;
+		}
+		cout << "Number contains" << counter << "digits\n";
+	}
